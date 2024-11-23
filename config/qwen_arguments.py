bba2cbfe549064f73cac87d98b6b19eca5e2d7bc
pica-git0 <jilp1598.git@gmail.com>
2024-11-24 07:32:20 +0900

    feat: eval_strategy 기본값을 steps에서 epoch으로 변경
    과하게 긴 evaluation 실행 시간으로 인해 evaluation이 최종 한 번만 실행되도록 변경

--- config/qwen_arguments.py
+++ config/qwen_arguments.py
@@ -31,4 +31,4 @@ class Qwen32BwithUnsloth_DataTrainingArguments(DataTrainingArguments):
     max_steps: int = field(default=30, metadata={"help": "train을 진행할 최대 step 수 입니다."})
     save_steps: int = field(default=3, metadata={"help": "체크포인트를 저장할 step 수입니다. (save_steps 진행 별 체크포인트 저장 - ex. 3 step 진행 후 저장, 6 step 진행 후 저장)"})
     save_strategy: str = field(default="steps", metadata={"help": "checkpoint save 정책을 지정합니다. (e.g. steps, epoch)"})
-    eval_strategy: str = field(default="steps", metadata={"help": "eval 정책을 지정합니다. (e.g. steps, epoch)"})
+    eval_strategy: str = field(default="epoch", metadata={"help": "eval 정책을 지정합니다. (e.g. steps, epoch)"})
