c2d62707b8fd702355216868eef7ed13de95582a
pica-git0 <jilp1598.git@gmail.com>
2024-11-27 09:16:53 +0900

    feat: is_augmented 인자 추가
    is_augmented : 증강되었는 지 여부를 전달
    
    데이터 증강 여부를 전달하여 기본 데이터셋와 증강 데이터셋의 차이를 고려하여 적절히 파싱하기 위해 추가

--- run/run_train_qwen_best.sh
+++ run/run_train_qwen_best.sh
@@ -3,4 +3,4 @@
 export PYTHONPATH=$PYTHONPATH:.
 export HUGGINGFACE_TOKEN=
 huggingface-cli login --token $HUGGINGFACE_TOKEN
-python scripts/train_qwen_best.py --config_path ./config/qwen2.5_32B_unsloth_best_config.yaml
+python scripts/train_qwen_best.py --config_path ./config/qwen2.5_32B_unsloth_best_config.yaml --is_augmented True
